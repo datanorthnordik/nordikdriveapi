0f480802d36fd1a13653321769df22aeaa366bce
Athul Narayanan <narayanan@algomau.ca>
2026-02-27 15:15:06 -0500

    Lookup package added for lookup(province, day school)

--- internal/lookup/routes.go
+++ internal/lookup/routes.go
@@ -7,7 +7,7 @@ import (
 func RegisterRoutes(r *gin.Engine, lookupService LookupServiceAPI) {
 	lookupController := &LookupController{Service: lookupService}
 
-	userGroup := r.Group("/lookup")
+	userGroup := r.Group("/api/lookup")
 	{
 		userGroup.GET("/province", lookupController.GetAllProvinces)
 		userGroup.GET("/dayschool/:province", lookupController.GetDaySchoolsByProvince)
