74ebd850897b9c811fa036cbcd53c0ed57297c30
Athul Narayanan <narayanan@algomau.ca>
2026-03-13 10:48:49 -0400

    test cased added for form submission

--- internal/formsubmission/port.go
+++ internal/formsubmission/port.go
@@ -6,21 +6,10 @@ type FormSubmissionServiceInterface interface {
 	GetByRowAndForm(rowID int64, formKey string, fileID *int64) (*GetFormSubmissionResponse, error)
 	Upsert(req *SaveFormSubmissionRequest, userID int) (*GetFormSubmissionResponse, error)
 	GetUploadBytes(id uint) ([]byte, string, string, error)
-	SearchSubmissions(
-		ctx context.Context,
-		req SearchFormSubmissionsRequest,
-		page int,
-		pageSize int,
-	) (*PaginatedFormSubmissionsResponse, error)
+	SearchSubmissions(ctx context.Context, req SearchFormSubmissionsRequest, page int, pageSize int) (*PaginatedFormSubmissionsResponse, error)
 	GetFormsByFileID(fileID int64) ([]FormFileMappingResponse, error)
 	ReviewSubmission(req *ReviewFormSubmissionRequest, reviewerID int) (*GetFormSubmissionResponse, error)
-	SearchMySubmissions(
-		ctx context.Context,
-		userID int,
-		req SearchFormSubmissionsRequest,
-		page int,
-		pageSize int,
-	) (*PaginatedFormSubmissionsResponse, error)
+	SearchMySubmissions(ctx context.Context, userID int, req SearchFormSubmissionsRequest, page int, pageSize int) (*PaginatedFormSubmissionsResponse, error)
 }
 
 var _ FormSubmissionServiceInterface = (*FormSubmissionService)(nil)
